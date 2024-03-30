3a501960de75a8ccac1bb89bd7861208536c90fc
Felix Hartnagel <felix@fhcom.de>
2024-03-30 15:38:08 +0100

    Init

---
+++ docs/intro.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+---
+# Einführung
+
+## Ziel dieser Seite 🎯
+
+Diese Seite hat das Ziel, das gesamte Prüfungswissen für die Fachinformatiker Berufe auf einer übersichtlichen Website darzustellen. Außerdem soll sie Open Source und von jedem erweiterbar sein. Gelöst wurde das ganze über GitHub. Wie genau du an diesem Projekt teilhaben kannst, wird im Unterpunkt [Contributing](./#contributing-) beschrieben. Der einfachste Punkt um zu dieser Seite beizutragen, ist aber sie bekannter zu machen. Wenn dir also gefällt, was wir hier erschaffen, dann erzähl es doch bitte allen Interessierten. Deine Berufsschulklasse oder Azubis im Betrieb werden sich sicher über ein bisschen Hilfe, bei der Prüfungsvorbereitung, freuen 😉
+
+## Geschichte 👴
+
+Als ich mich auf die Prüfung vorbereiten wollte, habe ich mir einige Notizen gemacht. Und genau aus diesen Notizen ging dann dieses Projekt hervor. Ich habe versucht so viel Wissen wie möglich zu sammeln und tue es auch nach wie vor. Die Website erhält deshalb auch regelmäßig neue Updates. Für die Zukunft erhoffe ich mir, dass an diesem Projekt so viele Fachinformatiker wie möglich teilhaben werden. Mithilfe von einigen Leuten kann so eine riesige Wissensdatenbank zur Prüfungsvorbereitung erstellt werden.
---
+++ docusaurus.config.js
@@ -0,0 +1,91 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'FI Prüfungsvorbereitung',
+  tagline: 'Sammlung von Prüfungsinhalten der Fachinformatiker Prüfung',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://fi.fhaf.de',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'fehart20', // Usually your GitHub org/user name.
+  projectName: 'fi-pruefung', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'FI Prüfungsvorbereitung',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Inhalte',
+          },
+          {
+            href: 'https://github.com/fehart20/fi-pruefung',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} FHCOM`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
